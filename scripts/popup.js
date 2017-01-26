9fdeef95f820efdf4020b58fc28daac2a1830c2a
Luc CLÉMENT <luc.clement421@gmail.com>
2017-01-26 13:07:23 +0100

    allow user to toggle autozen mode

---
+++ scripts/popup.js
@@ -0,0 +1,15 @@
+var autozenCheckbox = $('#autozen-checkbox');
+var autozen = false;
+
+chrome.storage.sync.get('autozen', function(result) {
+    autozen = !!result.autozen;
+    autozenCheckbox.prop('checked', autozen);
+});
+
+// Bind click
+autozenCheckbox.on('click', function() {
+    autozen = autozenCheckbox.prop('checked');
+    chrome.storage.sync.set({'autozen': autozen}, function() {
+        console.log('Autozen mode set to', autozen);
+    });
+  });
